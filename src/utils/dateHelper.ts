dba18a2ae896461256bb801a546bc745e3211821
Ninotter <nino.didier@live.fr>
2025-08-18 10:11:05 +0200

    Show dates without seconds

--- src/utils/dateHelper.ts
+++ src/utils/dateHelper.ts
@@ -30,7 +30,7 @@ export const getTimeAgo = (date: Date): string => {
 }
 
 export const formatDate = (date: Date): string => {
-    return date.toLocaleDateString() + " " + date.toLocaleTimeString();
+    return date.toLocaleDateString() + " " + date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit'});
 }
 
 export const useTimeAgo = (date: Date) => {
